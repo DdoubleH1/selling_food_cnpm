2d59946dcfa7303e9b8f9c735998c03377239568
DoHuyHoang <dohoang225@gmail.com>
2024-05-06 06:46:17 +0700

    init prj

---
+++ src/test/java/cnpm/hoangdh/selling_food/SellingFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package cnpm.hoangdh.selling_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellingFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
